4ad752cc805a91190fb2db08c152ed349e7c5c8f
Yikai <shuffle3105@gmail.com>
2022-08-21 18:38:45 +0800

    first commit

---
+++ app/assets/js/all.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  $('.navbar-toggler').click(function () { 
+      $('body').toggleClass('navbar-nav_show');
+  });
+});
+// Swiper
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
